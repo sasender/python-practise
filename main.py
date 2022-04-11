9c5c585da9b1a5769b34c38359a7f0d0ecd3310b
sasender <sasender123@gmail.com>
2022-04-11 15:51:29 +0530

    changes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-print ('hello-bhanti')
+print ('hello-jhon')
 
 a = 100
 b = 350 
 c = 450 
 d = 750
 movieticketfare = 100
-total = (a+b+c+d)
+total = a + b + c + d
 print = (total)
 print = (total/movieticketfare)
