1c87953e8e9f360a7749447f367de1e3e771338b
sasender <sasender123@gmail.com>
2022-04-11 19:23:44 +0530

    chnages

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print ('hello-jhon')
+'''print ('hello-jhon')
 
 a = 100
+a='nik'
+print(a)
 b = 350 
 c = 450 
 d = 750
@@ -8,3 +10,18 @@ movieticketfare = 100
 total = a + b + c + d
 print (total)
 print (total/movieticketfare)
+
+sum=10
+def calculate ():
+    currentSum=250
+    totalsum=sum+currentSum 
+    print (totalsum)
+calculate () ;  '''
+
+#programe demonstrate input and output
+import re
+
+
+cash = 1000
+remainingAmount = 10000
+print ('take your cash',cash,remainingAmount,sep="->")
