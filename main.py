478ec275e2463b636e5fb4c0a193b00d0e06d96a
sasender <sasender123@gmail.com>
2022-04-12 19:31:05 +0530

    new_code

--- main.py
+++ main.py
@@ -19,14 +19,28 @@ def calculate ():
 calculate () ;  '''
 
 #programe demonstrate input and output
-import re
+'''import re
 
 
 cash = int(input('enter your cash here'))
+#cashValue = int(cash)
 SavingsAmount = 10000
 remainingAmt=(SavingsAmount-cash)
 savingsAmt=remainingAmt
 print('type of cash is: ',type(cash))
 print ('take your cash',cash,remainingAmt,sep="->",end="===")
 print('thank you',end="\t")
-print('thanks sasender')
+print('thanks sasender')'''
+
+pin, cash = input('Enter Your Pin And Cash').split('@')
+print('pin and cash are',pin,cash)
+cashValue = int(cash)
+#print('Type of cash is: ',type(cashValue))
+atmAmount = 5000000
+remainingAmount = atmAmount-cashValue
+print('Take your cash',cash,remainingAmount,sep="@@",end="===")
+print('Thank You for using Atm',end="/t")
+print('Thanks You Sasender Reddy')
+
+
+
