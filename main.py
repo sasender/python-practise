23e2eae6b956ffa58c22561447aaab7b240b6695
sasender <sasender123@gmail.com>
2022-04-13 17:17:48 +0530

    code_added

--- main.py
+++ main.py
@@ -32,15 +32,40 @@ print ('take your cash',cash,remainingAmt,sep="->",end="===")
 print('thank you',end="\t")
 print('thanks sasender')'''
 
-pin, cash = input('Enter Your Pin And Cash').split('@')
-print('pin and cash are',pin,cash)
+'''pin, cash = input('Enter Your Pin And Cash').split('@')
+#print('pin and cash are',pin,cash)
 cashValue = int(cash)
 #print('Type of cash is: ',type(cashValue))
-atmAmount = 5000000
+atmAmount = 500
 remainingAmount = atmAmount-cashValue
-print('Take your cash',cash,remainingAmount,sep="@@",end="===")
-print('Thank You for using Atm',end="/t")
-print('Thanks You Sasender Reddy')
+print('Take your cash {1}, balance in your account is {0}'.format(cashValue,remainingAmount,end=" "))
+# print('Take your cash',cash,remainingAmount,sep="@@",end="===")
+# print('Thank You for using Atm',end=" ")
+print('Thank You Sasender Reddy')'''
+
+
+#chapter-11 {Operations}
+'''from ast import operator
+from doctest import OutputChecker
+
+
+Different type of operators 
+Arithmetic
+
+Relational operator
+
+Assignment
+
+Logical
+
+Bitwise'''
+
+#while discusing input-Output
+#using format we can write fancier out puts [place holder {}]
+
+
+#opeartors programe python
+
 
 
 
---
+++ opearators.py
@@ -0,0 +1,30 @@
+print('operatons')
+#Airthematic Operations
+'''a = 5
+b = 8
+print('adding', a + b)
+print('subStraction', a - b)
+print('mulTiply', a * b)
+print('divide', a / b)
+print('floring', a // b)
+
+print('reminder', a % b)
+
+print('power', a**b)'''
+
+#Assignment Operators
+# a = 15
+# b = 10
+# b**=5
+# print(b)
+
+#date of the bithday 
+from datetime import date
+yearOfBirth = int(input('Please tell Your date of Birth'))
+currentYear = date.today().year
+age = currentYear - yearOfBirth
+print('Your age is:',age)
+print(date.today().year)
+
+
+
